5cdc5e53300090347f8777ea17f47a1d63cbf109
ben9955 <ben95ansari@gmail.com>
2024-04-15 18:06:41 +0100

    created the route to get all the users

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParsel from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -15,6 +18,7 @@ app.use(cookieParsel()); //  to estract the cookies from the browser
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello world!!");
