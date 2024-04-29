fe6f84ebeef07b1eb73f8458cc6aa921e74e5070
ben9955 <ben95ansari@gmail.com>
2024-04-29 17:54:59 +0100

    final commit, added socket fro realtime messages

--- frontend/src/components/messagecontainer/Messages.jsx
+++ frontend/src/components/messagecontainer/Messages.jsx
@@ -1,24 +1,22 @@
 import { useEffect, useRef } from "react";
-import useGetMessages from "../../hook/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
+import useListenMessages from "../../hook/useListenMessages";
+import useGetMessages from "../../hook/useGetMessages";
 import Message from "./Message";
 
-function Messages() {
-  const { loading, messages } = useGetMessages();
+const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  useListenMessages();
   const lastMessageRef = useRef();
 
   useEffect(() => {
-    lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
-  });
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
 
   return (
-    <div className="flex-1 px-4 overflow-auto">
-      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-
-      {!loading && messages.length === 0 && (
-        <p>Send a message to start the conversation</p>
-      )}
-
+    <div className="px-4 flex-1 overflow-auto">
       {!loading &&
         messages.length > 0 &&
         messages.map((message) => (
@@ -26,8 +24,35 @@ function Messages() {
             <Message message={message} />
           </div>
         ))}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
     </div>
   );
-}
-
+};
 export default Messages;
+
+// STARTER CODE SNIPPET
+// import Message from "./Message";
+
+// const Messages = () => {
+// 	return (
+// 		<div className='px-4 flex-1 overflow-auto'>
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 			<Message />
+// 		</div>
+// 	);
+// };
+// export default Message
